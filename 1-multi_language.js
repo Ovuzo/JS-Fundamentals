fe3df3a79b3f3640297ae31fb232adc380e08715
Ovuzo <ovuzonyobeno@gmail.com>
2025-07-12 05:58:24 +0100

    added multi_language file

---
+++ 1-multi_language.js
@@ -0,0 +1,13 @@
+// Write a script that prints 3 lines:
+
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+let a = "C is fun";
+let b = "Python is cool";
+let c = "Javascript is amazing"
+console.log(a)
+console.log(b)
+console.log(c)
